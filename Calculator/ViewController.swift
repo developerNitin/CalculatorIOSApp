ce7434ec10612b0c12502820385b65e2cdf7ffcc
Nitin Birdi <nitin.birdi@icloud.com>
2020-05-10 20:36:41 +0530

    #3_commit: More Functionality to Our Calculator Added

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,7 +15,20 @@ class ViewController: UIViewController {
     private var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        isFinishedTypingNumber = true        
+        isFinishedTypingNumber = true
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("cannot convert display label text to double")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number/100)
+            }
+        }
     }
     
     
